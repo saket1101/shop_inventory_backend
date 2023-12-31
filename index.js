a5b728bd6fbca69b07709bfc723e897007c27c0b
Saket Kumar <saketjha00@gmail.com>
2023-12-31 12:52:29 +0530

    added cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectToDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 // route connection
 const userRouter = require('./routers/userRouter');
